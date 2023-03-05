f2e6fc4bdc4e30fc4a6b46a2f696d0eec3462b33
nawed2611 <nawed2611@gmail.com>
2023-03-05 15:21:46 +0530

    Add trim to choice

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,37 @@ use rand::thread_rng;
 use std::io;
 
 fn main() {
-    let mut choice = String::new();
-    let mut rng = thread_rng();
-    let choices = [String::from("r"), String::from("p"), String::from("s")];
-    let comp_choice = choices.choose(&mut rng).expect("Failed to choose");
-
     println!("Rock-Paper-Scissor Game");
-    println!("Enter (r)ock (p)aper or (s)cissor");
+    let mut game = true;
 
-    io::stdin()
-        .read_line(&mut choice)
-        .expect("Failed to read line");
+    while game {
+        println!("Enter (r)ock (p)aper or (s)cissor or (q)uit");
 
-    println!("You chose {}", choice);
-    println!("Computer chose {}", comp_choice);
+        let mut choice = String::new();
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
 
-    if &choice == comp_choice {
-        println!("It's a tie!");
-    } else if choice == "r" && comp_choice == "s" {
-        println!("You win!");
-    } else if choice == "p" && comp_choice == "r" {
-        println!("You win!");
-    } else if choice == "s" && comp_choice == "p" {
-        println!("You win!");
-    } else {
-        println!("You lose!");
+        println!("You played: {}", choice);
+
+        let choices = [String::from("r"), String::from("p"), String::from("s")];
+        let mut rng = thread_rng();
+        let comp_choice = choices.choose(&mut rng).expect("Empty Range");
+
+        println!("Computer played: {}", comp_choice);
+
+        if choice.trim() == comp_choice {
+            println!("You won, hehe");
+        } else if choice.trim() == "q" {
+            game = false;
+        } else if choice.trim() == "r" && comp_choice == "s" {
+            println!("You won, hehe");
+        } else if choice.trim() == "p" && comp_choice == "r" {
+            println!("You won, hehe");
+        } else if choice.trim() == "s" && comp_choice == "p" {
+            println!("You won, hehe");
+        } else {
+            println!("You lost, hehe");
+        }
     }
 }
